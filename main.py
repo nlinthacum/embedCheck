0778cc60be93663375cc78074d844d378de02d75
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2022-02-04 23:02:20 +0100

    collects input edges

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ class Node:
 #Node1 = Node(5)
 
 
+numEdges = int(input("Enter number of edges: "))
+edges = [[0 for x in range(2)] for y in range(numEdges)]
+
+for x in range(numEdges):
+    edges[x][0] = int(input("Enter starting node: "))
+    edges[x][1] = int(input("Enter ending node: "))
+
+print(edges)
+
