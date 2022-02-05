09fb6ab00efb71de36358b7a52b701bc1698eae9
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2022-02-05 22:35:32 +0100

    setup nodes

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 
-class Node:
-    def __init__(self, numberNodes):
-        self.points = [True] * numberNodes
-        print(self.points)
+def CollectInput():
+    numNodes = int(input("Enter number of nodes: "))
+    numEdges = int(input("Enter number of edges: "))
+    edges = [[0 for x in range(2)] for y in range(numEdges)]
+
+    for x in range(numEdges):
+        edges[x][0] = int(input("Enter starting node: "))
+        edges[x][1] = int(input("Enter ending node: "))
+    #print(edges)
+    return numNodes
+
+def SetupNodes():
+    nodesFree = [Node(numNodes) for i in range(numNodes)]
+    for i in range(numNodes):
+        print(nodesFree[i].points)
 
 
-#Node1 = Node(5)
+class Node():
+    def __init__(self, numNodes):
+        self.points = [True] * numNodes
+        #print(self.points)
 
 
-numEdges = int(input("Enter number of edges: "))
-edges = [[0 for x in range(2)] for y in range(numEdges)]
+if __name__ == '__main__':
+    numNodes = CollectInput()
+    SetupNodes()
 
-for x in range(numEdges):
-    edges[x][0] = int(input("Enter starting node: "))
-    edges[x][1] = int(input("Enter ending node: "))
 
-print(edges)
+
+
+
+
+
 
